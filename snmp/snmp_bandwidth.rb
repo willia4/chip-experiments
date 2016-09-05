afcf98adf5cb8b225ab6e2096643132076ae8150
James Williams <james@jameswilliams.me>
2016-09-05 17:04:17 -0400

    Move the ring buffer out to its own utility file

--- snmp/snmp_bandwidth.rb
+++ snmp/snmp_bandwidth.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'snmp'
+require './ring_buffer.rb'
 
 # the host name or IP address of the the device to interrogate over snmp
 SNMP_AGENT = 'router1'    
@@ -23,33 +24,7 @@ SMOOTHING_FACTOR = 10
 # A larger delay gives worse results but is also better for battery life
 SAMPLE_DELAY_SECONDS = 1.0 
 
-class RingBuffer < Array 
-  attr_reader :max_size
 
-  def initialize(max_size)
-    @max_size = max_size
-    max_size.times { self << 0 }
-  end
-
-  def <<(el)
-    if self.size < @max_size
-      super
-    else
-      self.shift
-      self.push(el)
-    end
-  end
-
-  def sum
-    return self.inject(0) { |sum, el| sum + el }
-  end
-
-  def mean 
-    return self.sum / @max_size
-  end
-
-  alias :push :<<
-end
 
 class SpeedGetter < Object
   attr_reader :snmp_agent
