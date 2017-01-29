1877bd67fa54a17a678b54bd2e4728b949237367
James Williams <james@jameswilliams.me>
2017-01-29 01:50:32 +0000

    Use a weighted average for SNMP meter

--- bandwidth_meter/snmp_bandwidth.rb
+++ bandwidth_meter/snmp_bandwidth.rb
@@ -33,6 +33,8 @@ class SpeedGetter < Object
   attr_reader :snmp_counter
 
   def initialize(snmp_agent, snmp_community, snmp_counter, num_samples)
+    raise ArgumentError, "num_samples must be greater than 1" if num_samples <= 1
+
     @snmp_agent = snmp_agent
     @snmp_community = snmp_community
     @snmp_counter = snmp_counter
@@ -43,6 +45,10 @@ class SpeedGetter < Object
 
     @last_sample = 0
 
+    # Create a phi-weighted average
+    phi = ((1 + Math.sqrt(5))/ 2.0)
+    @weights = [1]
+    (num_samples - 1).times { @weights << @weights.last / phi } #num_samples - 1 because the first weight is always 1
 
     @samples = RingBuffer.new(num_samples)
     num_samples.times { @samples << {:time => Time.now, :bytes => get_current_bytes} }
@@ -58,19 +64,54 @@ class SpeedGetter < Object
     end
   end
 
+  #the @samples array doesn't have speeds in it; it has byte counts and timestamps
+  #to figure out the speeds, you need to look at each sample and figure out how many 
+  #bytes moved per unit time
+  #
+  #returns an array of bits-per-second values
+  def get_bps_from_samples()
+    bps_sum = 0
+    total_weight = 0 
+
+    #start with the second sample
+    #and work forward from there
+    #so we always have a previous to work from
+    #work in reverse because our weights are reversed
+
+    (1...(@samples.size)).reverse_each do |i|
+      prev = @samples[i - 1]
+      cur = @samples[i]
+
+      diff_bits = (cur[:bytes] * 8) - (prev[:bytes] * 8)
+      diff_seconds = cur[:time] - prev[:time]
+
+      bps = diff_bits / diff_seconds
+      bps = 0 if bps < 0
+
+      weight = @weights[i - 1]
+      total_weight += weight
+      weighted_bps =  weight * bps 
+
+      bps_sum += weighted_bps
+    end
+
+    return bps_sum / total_weight
+  end
+
   def get_current_speed()
     new_sample = {:time => Time.now, :bytes => get_current_bytes}
 
-    min = @samples.min_by { |s| s[:time] }
+    # min = @samples.min_by { |s| s[:time] }
     @samples << new_sample
     
-    diff_bytes = new_sample[:bytes] - min[:bytes]
-    diff_seconds = new_sample[:time] - min[:time]
+    # diff_bytes = new_sample[:bytes] - min[:bytes]
+    # diff_seconds = new_sample[:time] - min[:time]
 
-    bits_per_second = (diff_bytes * 8) / diff_seconds
+    # bits_per_second = (diff_bytes * 8) / diff_seconds
 
-    bits_per_second = 0 if bits_per_second < 0
+    # bits_per_second = 0 if bits_per_second < 0
 
+    bits_per_second = get_bps_from_samples()
     kilobits_per_second = bits_per_second / 1000.0
     megabits_per_second = kilobits_per_second / 1000.0
 
